61204b2b0731262ef4b5c9481ae73de1d391eb3e
Aarav-Nigam <avatar.aarav28@gmail.com>
2024-03-29 20:07:54 +0530

    adding background

--- app.py
+++ app.py
@@ -35,6 +35,19 @@ st.set_page_config(
     page_icon=":robot_face:",
     layout="wide"
 )
+
+# Background image
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background: url("https://images.unsplash.com/photo-1620641788421-7a1c342ea42e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D") center center;
+        background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 # Streamlit app
 st.title("Image Classification \n(ResNet Architecture)")
 
